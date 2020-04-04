fc226137292af0fe74e1a8f9a688d03c21273325
Darwin Gualotuña <daosgc@gmail.com>
2020-04-04 14:45:08 -0500

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,15 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :options, :delete, :put],
+      :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client']
+  end
+end
