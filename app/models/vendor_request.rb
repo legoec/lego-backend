3c45fbe2d35af48680b1dd952aec8f907bb905bc
Darwin Gualotuña <daosgc@gmail.com>
2020-04-19 17:03:27 -0500

    annotate

--- app/models/vendor_request.rb
+++ app/models/vendor_request.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: vendor_requests
+#
+#  id         :bigint           not null, primary key
+#  status     :string
+#  feedback   :text
+#  vendor_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class VendorRequest < ApplicationRecord
   belongs_to :vendor
 end
