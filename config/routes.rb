1f710a3344034b7901d5a178ad5ded3967f04fd8
Fabricio Flores <fabrifloresg@gmail.com>
2020-04-26 08:52:19 -0500

    Allow create vendor request

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
         end
       end
       resources :categories, only: [:index, :create, :show, :update]
+      resources :vendors, only: [:create]
     end
   end
 end
