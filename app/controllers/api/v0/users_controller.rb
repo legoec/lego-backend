27358682b03eca9b464ccf8aa2bde6c1faf40703
Darwin Gualotuña <daosgc@gmail.com>
2020-05-18 16:26:23 -0500

    Add api to update profile user

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,7 +1,8 @@
 module Api
     module V0
         class UsersController < ApplicationController
-          before_action :authenticate_user!, only: [:index, :vendor]
+          before_action :authenticate_user!, only: [:index, :vendor, :update]
+          before_action :find_user, only: [:update]
 
           def index
               @users = User.all
@@ -15,6 +16,25 @@ module Api
               # render an empty object to evaluate on frontend
               render json: {}
             end
+          end
+
+          def update
+            if !@user.update(user_params)
+              render 'errors/model_errors', status: :unprocessable_entity, locals: { errors: @user.errors }
+            end
+          end
+
+          private
+          def user_params
+            params.require(:user).permit(
+              :name,
+              :nickname,
+              :email
+            )
+          end
+
+          def find_user
+            @user = User.find(params[:id])
           end
         end
     end
