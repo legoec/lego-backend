b366750b944cb2269dcf3d78c53e5b1878c4b7aa
Darwin Gualotuña <daosgc@gmail.com>
2020-06-01 12:10:14 -0500

    Add api to update profile user (#41)
    * Add api to update profile user
    
    * Limit search just to current user

--- app/controllers/api/v0/users_controller.rb
+++ app/controllers/api/v0/users_controller.rb
@@ -1,7 +1,8 @@
 module Api
     module V0
         class UsersController < ApplicationController
-          before_action :authenticate_user!, only: [:index, :vendor]
+          before_action :authenticate_user!, only: [:index, :vendor, :update]
+          before_action :find_me, only: [:update]
 
           def index
               @users = User.all
@@ -15,6 +16,25 @@ module Api
               # render an empty object to evaluate on frontend
               render json: {}
             end
+          end
+
+          def update
+            if !@user.update(user_params)
+              render 'errors/model_errors', status: :unprocessable_entity, locals: { errors: @user.errors }
+            end
+          end
+
+          private
+          def user_params
+            params.require(:user).permit(
+              :name,
+              :nickname,
+              :email
+            )
+          end
+
+          def find_me
+            @user = @current_user
           end
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v0 do
-      resources :users do
+      resources :users, only: [:update] do
         member do
           get :vendor
         end
